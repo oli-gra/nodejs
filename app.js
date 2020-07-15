008c2540b2aa74965a96519296d53c8a26b24359
oli-gra <oliver.grant@gmail.com>
2020-07-15 20:16:51 +0100

    express refactor

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const http = require('http')
-const routes = require('./routes')
+const express = require('express')
+const parser = require('body-parser')
 
-const server = http.createServer(routes)
+const app = express()
+app.use(parser.urlencoded({ extended: false }))
 
-server.listen(3000)
+app.use('/added', (req, res, next) => {
+  console.log(req.body)
+  res.redirect('/')
+})
+app.use('/add', (req, res, next) => {
+  res.send('<form action="/added" method="POST"><input name="add"><button type="submit">ADD</button></form>').status(200)
+})
+app.use('/', (req, res, next) => {
+  res.send('<h1>hello</h1>').status(200)
+})
+
+app.listen(3000)
